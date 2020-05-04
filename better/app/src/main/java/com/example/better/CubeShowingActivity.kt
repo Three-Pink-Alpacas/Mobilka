c06f057f16d005f580fbe3c3524d0f599133e9ee
Krivelev Daniil <krivelevdaniil@gmail.com>
2020-05-05 01:09:27 +1000

    surface view is not fullscreen now. added button fpr getting back

--- better/app/src/main/java/com/example/better/CubeShowingActivity.kt
+++ better/app/src/main/java/com/example/better/CubeShowingActivity.kt
@@ -2,6 +2,7 @@ package com.example.better
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
@@ -9,50 +10,19 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import android.view.Window
+import android.view.View
 import java.util.*
 
 
 class CubeShowingActivity : Activity() {
+    private var mSurface: SurfaceView? = null
     public override fun onCreate(icicle: Bundle?) {
         super.onCreate(icicle)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(MySurfaceView(this))
+        setContentView(R.layout.activity_cube_showing)
+        mSurface = findViewById(R.id.surface);
     }
-
-    internal inner class MySurfaceView(context: Context?) :
-        SurfaceView(context) {
-        var path: Path? = null
-        var thread: Thread? = null
-        var surfaceHolder: SurfaceHolder
-
-        @Volatile
-        var running = false
-        private val paint =
-            Paint(Paint.ANTI_ALIAS_FLAG)
-        var random: Random? = null
-        override fun onTouchEvent(event: MotionEvent): Boolean {
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                path = Path()
-                path!!.moveTo(event.x, event.y)
-            } else if (event.action == MotionEvent.ACTION_MOVE) {
-                path!!.lineTo(event.x, event.y)
-            } else if (event.action == MotionEvent.ACTION_UP) {
-                path!!.lineTo(event.x, event.y)
-            }
-            if (path != null) {
-                val canvas = surfaceHolder.lockCanvas()
-                canvas.drawPath(path!!, paint)
-                surfaceHolder.unlockCanvasAndPost(canvas)
-            }
-            return true
-        }
-
-        init {
-            surfaceHolder = holder
-            paint.style = Paint.Style.STROKE
-            paint.strokeWidth = 3f
-            paint.color = Color.WHITE
-        }
+    fun mainMenuMove(view: View) {
+        val intent = Intent(this, MainActivity::class.java);
+        startActivity(intent);
     }
 }
