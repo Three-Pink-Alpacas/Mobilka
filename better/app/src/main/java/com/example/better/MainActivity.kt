58fbf25a0dc628431f8ce1c0868ec3a2a049128f
Krivelev Daniil <krivelevdaniil@gmail.com>
2020-05-06 20:15:13 +1000

    added button "open in gallery". it allows user to open image from the storage

--- better/app/src/main/java/com/example/better/MainActivity.kt
+++ better/app/src/main/java/com/example/better/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.better
 
 import android.Manifest.permission.*
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -15,6 +19,28 @@ class MainActivity : AppCompatActivity() {
 
     private val neededPermissions = arrayOf(CAMERA, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE)
     private val REQUEST_CODE = 100
+
+    private var selectedImage: ImageView? = null
+    private var currentImage: Bitmap? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        selectedImage = findViewById(R.id.selectedImage);
+        checkPermissions()
+    }
+
+    fun cubeMove(view: View) {
+        val intent = Intent(this, CubeShowingActivity::class.java);
+        startActivity(intent);
+    }
+    fun plusMove(view: View) {}
+    fun galleryOpen(view: View) {
+        val photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, 1)
+    }
 
     private fun checkPermissions() {
         val currentAPIVersion = Build.VERSION.SDK_INT
@@ -34,16 +60,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        checkPermissions()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            val photoUri = data!!.data
+            if (photoUri != null) {
+                try {
+                    currentImage = MediaStore.Images.Media.getBitmap(this.contentResolver, photoUri)
+                    selectedImage!!.setImageBitmap(currentImage)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 
-    fun cubeMove(view: View) {
-        val intent = Intent(this, CubeShowingActivity::class.java);
-        startActivity(intent);
+    override fun onStop() {
+        super.onStop()
+        if (currentImage != null) {
+            currentImage!!.recycle()
+            currentImage = null
+            System.gc()
+        }
     }
-    fun plusMove(view: View) {}
-
 }
