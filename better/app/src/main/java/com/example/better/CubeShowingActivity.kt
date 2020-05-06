712398fff75de0bd6f73d9bcc66baa387ced5434
Krivelev Daniil <krivelevdaniil@gmail.com>
2020-05-06 19:11:30 +1000

    added empty button "+", now app requires permissions for camera and storage (read + write)

--- better/app/src/main/java/com/example/better/CubeShowingActivity.kt
+++ better/app/src/main/java/com/example/better/CubeShowingActivity.kt
@@ -1,28 +1,30 @@
 package com.example.better
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
+import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
-import java.util.*
 
 
 class CubeShowingActivity : Activity() {
     private var mSurface: SurfaceView? = null
+    private var mThread: Thread? = null
+
     public override fun onCreate(icicle: Bundle?) {
         super.onCreate(icicle)
         setContentView(R.layout.activity_cube_showing)
         mSurface = findViewById(R.id.surface);
+
     }
+    // button for getting in main menu
     fun mainMenuMove(view: View) {
         val intent = Intent(this, MainActivity::class.java);
         startActivity(intent);
     }
+
+
 }
--- better/app/src/main/java/com/example/better/MainActivity.kt
+++ better/app/src/main/java/com/example/better/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.better
 
+import android.Manifest.permission.*
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val neededPermissions = arrayOf(CAMERA, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE)
+    private val REQUEST_CODE = 100
+
+    private fun checkPermissions() {
+        val currentAPIVersion = Build.VERSION.SDK_INT
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            val permissionsNotGranted = ArrayList<String>()
+            for (permission in neededPermissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    permissionsNotGranted.add(permission)
+                }
+            }
+            val arr = arrayOfNulls<String>(permissionsNotGranted.size)
+            if (permissionsNotGranted.size > 0) {
+                ActivityCompat.requestPermissions(this@MainActivity, permissionsNotGranted.toArray(arr), REQUEST_CODE)
+            }
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermissions()
     }
+
     fun cubeMove(view: View) {
         val intent = Intent(this, CubeShowingActivity::class.java);
         startActivity(intent);
     }
+    fun plusMove(view: View) {}
+
 }
